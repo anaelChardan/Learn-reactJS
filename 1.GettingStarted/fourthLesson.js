d8c89a6d2a906a34d3e8aa9629b730010efbec5d
Anael CHARDAN <anael.chardan@gmail.com>
2017-05-27 23:23:59 +0200

    Fourth Lesson

---
+++ 1.GettingStarted/fourthLesson.js
@@ -0,0 +1,78 @@
+const Card = (props) => {
+    return (
+        <div>
+            <img width="75" style={{margin: '1em'}} src={props.avatar_url}/>
+            <div style={{display: 'inline-block', marginLeft: 10}}>
+                <div style={{fontSize: '1.25em', fontWeight: 'bold'}}>{props.name}</div>
+                <div>{props.company}</div>
+            </div>
+        </div>
+    )
+}
+
+const CardList = (props) => {
+		return (
+        <div>
+            {props.cards.map(card => 
+                <Card key={card.id} {...card}/>
+                )
+            }
+        </div>
+    )
+}
+
+class Form extends React.Component {
+    state = { userName: '' }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        axios.get(`https://api.github.com/users/${this.state.userName}`).then(resp => {
+            if (200 === resp.status) {
+                 this.props.onSubmit(resp.data)
+                 this.setState(prevState => ({
+                     userName: ''
+                 }))
+            }
+        })
+        
+    }
+    
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <input 
+                    value={this.state.userName}
+                    onChange={(event) => this.setState({ userName: event.target.value})}
+                    type="text" 
+                    placeholder="Github Username"
+                    required
+                />
+                <button type="submit">Add Card</button>
+            </form>
+        )
+    }
+}
+
+class App extends React.Component {
+    state = {
+        cards: []
+    }
+    
+    addNewCard = (cardInformation) => {
+    	this.setState(prevState => ({
+          cards: prevState.cards.concat(cardInformation)
+      }))
+    }
+    
+    render() {
+        return (
+            <div>
+                <Form onSubmit={this.addNewCard}/>
+                <CardList cards={this.state.cards}/>
+            </div>
+        )
+    }
+}
+
+
+ReactDOM.render(<App />, mountNode)
